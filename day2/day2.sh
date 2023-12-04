f7e8163d917185be2b97b7859879ba2b7c07db29
onesmallskipforman <skipper.gonzalez@icloud.com>
2023-12-04 14:09:53 -0500

    Solve day 1

--- day2/day2.sh
+++ day2/day2.sh
@@ -1,23 +1,43 @@
 #!/bin/zsh
 
-
-
-# only 12 red cubes, 13 green cubes, and 14 blue cubes
-
-MAXRED=12
-MAXGREEN=13
-
-
-# if not exceeded, print game number
-
-
-aboveMax() {
-    COLOR=$1
-    LIMIT=$2
-    cat input.txt | head -n1 | grep -o "[1-9]* $COLOR" | awk "{s = s || (\$1 > $LIMIT)? 1:0;next} END {print s}"
+processRow() {
+    local DATA=$1
+    local COLOR=$2
+    local MAX=$3
+    echo $DATA | sed -r "s/([0-9]* )$COLOR|./\1/g; s/:/ /g" | awk "{s=0;for(i=1; i<=NF; i++) {s=s||(\$i>$MAX)} print s}"
 }
 
-aboveMax 'blue' 14
+# This could/should be faster. For G games and C colors this program will run GxC shubshells
+processColors() {
+    while read -r data; do
+        local ROWNUM=$(echo $data | sed -r 's/^Game ([0-9]*):.*/\1/g')
+        local B=$(processRow $data "blue" 14)
+        local R=$(processRow $data "red" 12)
+        local G=$(processRow $data "green" 13)
+        # echo $R $G $B
+        [[ $R == 0 ]] && [[ $G == 0 ]] && [[ $B == 0 ]] && echo $ROWNUM
+    done
+}
 
-# MAXBLUE=14
-# head -n1 | grep -o '[1-9]* blue' | awk "{(\$1 > s)? \$1 : s;next} END {print s}"
+rowMin() {
+    local DATA=$1
+    local COLOR=$2
+    echo $DATA | sed -r "s/([0-9]* )$COLOR|./\1/g; s/:/ /g" | awk '{s=0;for(i=1; i<=NF; i++) {s=($i>s)? $i:s} print s}'
+}
+
+processMins() {
+    while read -r data; do
+        local ROWNUM=$(echo $data | sed -r 's/^Game ([0-9]*):.*/\1/g')
+        local B=$(rowMin $data "blue")
+        local R=$(rowMin $data "red")
+        local G=$(rowMin $data "green")
+        echo $R $G $B | awk '{s=$1; for(i=2; i<=NF; i++) {s=s*$i} print s}'
+        # [[ $R == 0 ]] && [[ $G == 0 ]] && [[ $B == 0 ]] && echo $ROWNUM
+    done
+}
+
+# TODO: pull awk summation helper out of this
+PART1=$(cat input.txt | processColors | awk '{s+=$1} END {print s}')
+PART2=$(cat input.txt | processMins   | awk '{s+=$1} END {print s}')
+echo "Part 1: $PART1"
+echo "Part 2: $PART2"
